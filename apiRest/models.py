e1b944c0a0fe5cb4d14d28a9c86b8f886066f6e7
Ronalses Aguilar <ronalses.developer@gmail.com>
2018-03-18 10:59:27 -0300

    models

--- apiRest/models.py
+++ apiRest/models.py
@@ -1,3 +1,30 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+# These models do not validate :/
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100, blank=True, default='user')
+    lastname = models.CharField(max_length=100, blank=True, default='')
+
+    class Meta:
+        ordering = ('id', )
+
+
+class Task(models.Model):
+    POSSIBLE_STATES = (
+        (0, 'Pending'),
+        (1, 'In process'),
+        (2, 'Finished'),
+    )
+
+    title = models.CharField(max_length=100, default="New Task")
+    status = models.IntegerField(default=0, choices=POSSIBLE_STATES)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ('-created', )
